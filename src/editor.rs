9d3c438bf3468998a9a7bb1663d1cb99ebb9f529
SySyAli <syedansabali786@gmail.com>
2024-05-16 11:00:09 -0500

    Adding tildes

--- src/editor.rs
+++ src/editor.rs
@@ -1,31 +1,70 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char};
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode, size, Clear, ClearType};
+use crossterm::cursor::MoveTo;
+use std::io::stdout;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
     pub fn default() -> Self {
-        Editor{}
+        Editor{ should_quit: false }
     }
-    pub fn run(&self){
-        enable_raw_mode().unwrap();
+    pub fn run(&mut self){
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    match event.code {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        },
-                        _ => (),
-                    }
-                },
-                Err(err) => println!("Error: {}", err),
-                _ => ()
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit {
+                self.draw_row();
+                break;
             }
         }
-        disable_raw_mode().unwrap();
+        Ok(())
+    }
+    fn evaluate_event(&mut self, event: &Event){
+        if let Key(KeyEvent{ code, modifiers, ..}) = event {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                },
+                _ => (),
+            }
+        }
+    }
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
+        }
+        Ok(())
+    }
+    fn draw_row(&self) -> Result<(), std::io::Error>{
+        let terminal_size = size()?;
+        MoveTo(0,0);
+        for num in 0..(terminal_size.1) {
+            println!("~\r");
+        }
+        MoveTo(0,0);
+        Ok(())
     }
 }
